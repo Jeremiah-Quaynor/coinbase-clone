89bafb85ba915ed0f808a7dffa4f657de8a860da
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2021-06-27 00:07:03 +0000

    adding functionality to the navbar

---
+++ script.js
@@ -0,0 +1,27 @@
+const navBar = document.querySelector('.navbar');
+const content = document.querySelector('.content');
+const minitab = document.querySelector('.minitab');
+
+minitab.addEventListener('mouseover',(event) =>{
+    const render = `
+        <section class='tab comot' >
+            <ul class="commot">
+                <li><a href="#">Food </a></li>
+                <li><a href="#">Boobs </a></li>
+                <li><a href="#">Breast </a></li>
+                <li><a href="#">Money </a></li>
+                <li><a href="#">More Food </a></li>
+                <li><a href="#">Movies </a></li>
+                <li><a href="#">Anime </a></li>
+            </ul>
+        </section>`;
+    if(document.querySelector(".tab")) return;
+    navBar.insertAdjacentHTML('afterend',render);
+})
+
+
+minitab.addEventListener('mouseout',(event) =>{
+    if( !document.querySelector(".tab")) return;
+    document.querySelector(".tab").remove()
+    
+})
