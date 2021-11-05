e3cd9aca73bb4b7c48f14c1770aca606d7a7a919
Jeremiah Quaynor <73517688+Jeremiah-Quaynor@users.noreply.github.com>
2021-11-05 12:34:03 +0000

    Tab fixed

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ minitab.addEventListener('mouseover',(event) =>{
     const render = `
         <section class='tab comot' >
             <ul class="commot">
-                <li><a href="#">Food </a></li>
-                <li><a href="#">Boobs </a></li>
-                <li><a href="#">Breast </a></li>
-                <li><a href="#">Money </a></li>
-                <li><a href="#">More Food </a></li>
+                <li><a href="#">Books</a></li>
+                <li><a href="#">Sites </a></li>
+                <li><a href="#">Articles </a></li>
+                <li><a href="#">Works </a></li>
+                <li><a href="#">More </a></li>
                 <li><a href="#">Movies </a></li>
-                <li><a href="#">Anime </a></li>
+                <li><a href="#">Technology </a></li>
             </ul>
         </section>`;
     if(document.querySelector(".tab")) return;
@@ -38,5 +38,5 @@ const obsOptions = {
     threshold: 0.1,
 };
 
-const observer = new IntersectionObserver(obsCallback,obsOptions);
-observer.observe(navBar);
+// const observer = new IntersectionObserver(obsCallback,obsOptions);
+// observer.observe(navBar);
